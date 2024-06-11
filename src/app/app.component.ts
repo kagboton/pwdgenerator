752786bd2e8b7dc960fc4297552b6eeb0e44e922
Kilian Agboton <agbotonstefane@gmail.com>
2024-06-11 12:41:37 -0400

    change event handler

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,28 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   generatedPassword: string = '';
+  includeLetters: boolean = false;
+  includeNumbers: boolean = false;
+  includeSymbols: boolean = false;
 
   onButtonClick() {
     this.generatedPassword = 'My password';
+    console.log(`
+      Include Letters ? : ${this.includeLetters}
+      Include N ? : ${this.includeNumbers};
+      Include S ? : ${this.includeSymbols}
+    `);
+  }
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeSymbols() {
+    this.includeSymbols = !this.includeSymbols;
   }
 }
